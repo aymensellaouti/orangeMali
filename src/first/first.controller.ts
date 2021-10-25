cd2de343f74eab666d8d15f98777d7c37b0277e6
aymen2782 <aymen.sellaouti@gmail.com>
2021-10-25 17:18:59 +0100

    Asynchrone and Observable

--- src/first/first.controller.ts
+++ src/first/first.controller.ts
@@ -1,4 +1,6 @@
 import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import { Observable, of } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Controller('first')
 export class FirstController {
@@ -7,24 +9,61 @@ export class FirstController {
     console.log('GET');
     return 'GET';
   }
+
   @Post('')
   showPost(): string {
     console.log('Post');
     return 'Post';
   }
+
   @Put('')
   showPut(): string {
     console.log('Put');
     return 'Put';
   }
+
   @Patch('')
   showPatch(): string {
     console.log('Patch');
     return 'Patch';
   }
+
   @Delete('')
   showDelete(): string {
     console.log('Delete');
     return 'Delete';
+  }
+
+  @Get('observable')
+  playWithObservable() {
+    const observable = new Observable<number>((observer) => {
+      let i = 5;
+      setInterval(() => {
+        if (!i) {
+          observer.complete();
+        }
+        observer.next(i--);
+      }, 1500);
+    });
+    observable.subscribe((val) => {
+      console.log(val);
+    });
+    observable
+      .pipe(
+        map((data) => data * 3),
+        filter((x) => x % 2 === 0),
+      )
+      .subscribe(
+        (data) => {
+          console.log(`Je viens de recevoir la valeur ${data}`);
+        },
+        (erreur) => {
+          console.log('j ai une erreur');
+        },
+        () => {
+          console.log('fin du traitement');
+        },
+      );
+    return of(2, 5, 4);
   }
 }
